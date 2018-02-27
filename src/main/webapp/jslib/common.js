66ed75decf1413c95fb7e6f8b20f1712404aa3ef
xiaowu <2457893105@qq.com>
2018-02-27 16:12:27 +0800

    提交内容

--- src/main/webapp/jslib/common.js
+++ src/main/webapp/jslib/common.js
@@ -2,44 +2,51 @@
  * 
  */
 
-	function hashmap(){
-	
-		var self = this,
-		
-		 hash = {},
-		
-		 keys = [],
-		
-		count = 0,
-		
-		 values = [];
-	
-		self.add = function(key,value){
-			try{
-				if( (typeof(key) === "string" && typeof(key) != "") || typeof(key) === "boolean" || typeof(key) === "number" ){
-					hash[key] = values;
-					count++
-					if(keys.indexOf(key) === -1){
-						keys.push(key);
-					}
-					if(values.indexOf(value) === -1){
-						values.push(value);
-					}
-				}else{
-					throw new exception("key is not primary type");	
+function hashmap(){
+
+	var self = this,
+
+	hash = {},
+
+	keys = [],
+
+	count = 0,
+
+	values = [];
+
+	self.add = function(key,value){
+		try{
+			if( (typeof(key) === "string" && typeof(key) != "") || typeof(key) === "boolean" || typeof(key) === "number" ){
+				hash[key] = values;
+				count++
+				if(keys.indexOf(key) === -1){
+					keys.push(key);
 				}
-			}catch(e){
-				console.log(e);
-			}finally{
-				return self;
+				if(values.indexOf(value) === -1){
+					values.push(value);
+				}
+			}else{
+				throw new exception("key is not primary type");	
 			}
+		}catch(e){
+			console.log(e);
+		}finally{
+			return self;
 		}
+	},
 	
+	self.each = function(){
+		for(i in keys){
+			console.log(hash[keys[i]]);
+		}
+		return self;
 	}
-	
-	$(document).ready(function () {
-		var hash = new hashmap();
-		hash = hash.add(1,2);
-		console.log(hash);
-		console.log("ok");
-	});
+
+}
+
+$(document).ready(function () {
+	var hash1 = new hashmap();
+	hash1 = hash1.add(1,2);
+//	hash1 = hash1.add(2,3);
+	hash1.each();
+});
