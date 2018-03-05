36821c78a559e2f684155e37c85d9f3d987d9332
xiaowu <2457893105@qq.com>
2018-03-05 14:48:18 +0800

    提交代码更新封装的hashmap内容增加remove的方法

--- src/main/webapp/jslib/common.js
+++ src/main/webapp/jslib/common.js
@@ -34,19 +34,52 @@ function hashmap(){
 			return self;
 		}
 	},
-	
+
 	self.each = function(){
 		for(i in keys){
 			console.log(hash[keys[i]]);
 		}
 		return self;
-	}
+	},
 
+	self.remove = function(key){
+		try{
+			
+			if( (typeof(key) === "string" && typeof(key) != "") || typeof(key) === "boolean" || typeof(key) === "number" ){
+
+				var value = hash[key];
+
+				count--;
+
+				for( key_item in keys){
+					if(key_item === key)
+					{
+						delete hash[key];
+					}
+				}
+
+				var key_index = keys.indexOf(key) ;
+
+				var value_index = values.indexOf(value);
+
+				keys = keys[0,key_index].concat(keys[key_index+1]);
+
+				values = values[0,value_index].concat(values[value_index+1]);
+
+			}else{
+				throw new exception("key is not primary type");	
+			}
+		}catch(e){
+			console.log(e);
+		}finally{
+			return self;
+		}
+	}
 }
 
 $(document).ready(function () {
 	var hash1 = new hashmap();
 	hash1 = hash1.add(1,2);
-//	hash1 = hash1.add(2,3);
+	hash1.remove(1);
 	hash1.each();
 });
